07d144543b2390ed613da2dc08a5d1cfc8ec5f23
muromtcev94 <113006434+muromtcev94@users.noreply.github.com>
2022-09-12 16:48:47 +0500

    Update test_script.sh

--- bash/test_script.sh
+++ bash/test_script.sh
@@ -15,9 +15,9 @@ echo -e "${YELLOW}Configuring Nginx${NC}"
 touch /etc/nginx/sites-available/test.local
 cat > /etc/nginx/sites-available/test.local <<\eof
 server {
-        listen 80; # порт, прослушивающий nginx
-        server_name    localhost; # доменное имя, относящиеся к текущему виртуальному хосту
-        root  /var/www/html; # каталог в котором лежит проект, путь к точке входа
+        listen 80;
+        server_name    localhost;
+        root  /var/www/html;
 
 
         index index.php;
@@ -40,7 +40,7 @@ server {
         location ~* \.php$ {
         try_files $uri = 404;
         fastcgi_split_path_info ^(.+\.php)(/.+)$;
-        fastcgi_pass unix:/var/run/php/php8.1-fpm.sock; # подключаем сокет php-fpm
+        fastcgi_pass unix:/var/run/php/php8.1-fpm.sock;
         fastcgi_index index.php;
         fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
         include fastcgi_params;
